f768e58df1ab79186e21f24dd4a5083981eb9434
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-06-12 12:28:22 -0600

    delete .skips

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,24 +8,24 @@ describe('Main spec', () => {
     cy.visit('http://localhost:3000/')
   })
 
-  it.skip('should display the title upon loading the main page', () => {
+  it('should display the title upon loading the main page', () => {
     cy.get('h1').should('exist').should('be.visible') 
       .contains('URL Shortener')
   })
 
-  it.skip('should display the already made URL card on the main page', () => {
+  it('should display the already made URL card on the main page', () => {
     cy.get('.url').should('have.length', 2)
       .get('h3').contains('Awesome TEST photo')
     cy.get(':nth-child(2) > h3').contains('Awesome TEST photo NUMBER TWO!')
   })
 
-  it.skip('should display the form on the main page', () => {
+  it('should display the form on the main page', () => {
     cy.get('input[type="text"]').eq(0).should('be.visible')
     cy.get('input[type="text"]').eq(1).should('be.visible')
     cy.get('button').eq(0).contains('Shorten Please!')
   })
 
-  it.skip('should display the user-input in the inpit fields', () => {
+  it('should display the user-input in the inpit fields', () => {
     cy.get('input[type="text"]').eq(0).type('hello!')
       .should('have.value', 'hello!')
     cy.get('input[type="text"]').eq(1).type('websitedotcom!')
